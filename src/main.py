bae212abf5ba6c207ec1e250898d3869b56a956a
thidspessoa <thiagopessoa246@gmail.com>
2026-01-26 13:24:43 -0300

    feat(extraction-instagram): Finalizada implementação da extração dos dados dos perfis do instagram (Sem etapa de salvamento no excel)

--- src/main.py
+++ src/main.py
@@ -52,22 +52,22 @@ def main():
                     
                     # Cria uma instancia do extrator de instagram
                     instagram_extractor: InstagramExtractor = InstagramExtractor(browser, site)
-                    instagram_extractor.extract_html() # Metodo para extrair os dados do perfil
+                    html_text: dict = instagram_extractor.extract_html() # Metodo para extrair os dados do perfil em formato de texto
 
                     # # Executa a extração de contatos
-                    # instagram_extractor.extract_contacts()
+                    response_ia_instagram: dict = instagram_extractor.extract_contacts(html_text) # Metodo para extrair os contatos do html usando IA
+                    
+                    # Chamada de metodo para salvar os dados extraidos em planilha excell
                     
                 # Se for a landing page da empresa
                 else:
-                    
-                    continue # Stand-by para evitar execuções acidentais, pois vou desenvolver esse módulo ainda posteriormente
                     
                     ColorManager.info('Iniciando extração de dados da landing page...')
                     
                     # Cria uma instancia do extrator de website genérico
                     website_extractor: WebSiteExtractor = WebSiteExtractor(browser, site)
                     data_returned: dict = website_extractor.extract_html() # Metodo para extrair os dados do site
-                    response_ia: dict = website_extractor.extract_contacts(data_returned) # Metodo para extrair os contatos do html usando IA
+                    response_ia_site: dict = website_extractor.extract_contacts(data_returned) # Metodo para extrair os contatos do html usando IA
                     
                     # Chamada de metodo para salvar os dados extraidos em planilha excell
 
@@ -91,6 +91,7 @@ def main():
 
             # Retorna uma saída de print de sucesso padronizada
             ColorManager.success('Automação concluída com sucesso.')
+
 
         # Finaliza o navegador utilizando o metodo kill browser
         webdriver.kill_browser()
